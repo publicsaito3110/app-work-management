05879ec4a9f42c0de783b75792544f08badf51c8
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-24 12:06:11 +0900

    first commit

---
+++ src/main/java/com/workManagement/AppWorkManagementApplication.java
@@ -0,0 +1,17 @@
+package com.workManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author saito
+ *
+ */
+@SpringBootApplication
+public class AppWorkManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWorkManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workManagement/AppWorkManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWorkManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
