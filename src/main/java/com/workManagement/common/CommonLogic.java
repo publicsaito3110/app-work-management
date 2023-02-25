9973ecf9c3254d3e8b72094649dd590a6b6e58ea
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-25 20:21:55 +0900

    fix: バリデーション判定共通処理の追加

---
+++ src/main/java/com/workManagement/common/CommonLogic.java
@@ -0,0 +1,41 @@
+package com.workManagement.common;
+
+/**
+ * @author saito
+ *
+ */
+public class CommonLogic {
+
+
+	/**
+	 * バリデーション判定処理
+	 *
+	 * <p>
+	 * バリデーションを正規表現で判定し、バリデーションの結果を返す
+	 * ただし、正規表現パターンと一致していないまたは正規表現に則していないときは必ず失敗する
+	 * </p>
+	 *
+	 * @param value 全ての値
+	 * @param regex 正規表現パターン
+	 * @return boolean<br>
+	 * true: バリデーションが成功(正規表現に基づいている)<br>
+	 * false: バリデーションが失敗(正規表現に基づいていないまたは引数が異常なとき)
+	 */
+	public boolean isSuccessValidation(String value, String regex) {
+
+		try {
+
+			// 正規表現パターンと一致したとき、trueを返す
+			if (value.matches(regex)) {
+				return true;
+			}
+
+			// 正規表現パターンと一致しなかったとき、falseを返す
+			return false;
+		} catch (Exception e) {
+
+			//例外発生時、falseを返す
+			return false;
+		}
+	}
+}
