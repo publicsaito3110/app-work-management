defe9a09738c2f27dfb1440fb79b015b631962ca
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-25 20:22:53 +0900

    add: セッションキーを定数に追加

---
+++ src/main/java/com/workManagement/common/Const.java
@@ -0,0 +1,19 @@
+package com.workManagement.common;
+
+/**
+ * @author saito
+ *
+ */
+public class Const {
+	private Const() {
+		//インスタンス化を禁止
+	}
+
+
+	//-------
+    // その他
+    //-------
+
+	// セッション
+	public static final String SESSION_KEYWORD_ACCOUNT_BEAN = "SESSION_KEY1_ACCOUNT_BEAN";
+}
