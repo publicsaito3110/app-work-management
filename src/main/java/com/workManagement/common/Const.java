aa6c76db9e290de6aba390eb5a2261e9adabe8b5
publicsaito3110 <public.business.saito3110@gmail.com>
2023-02-26 19:26:05 +0900

    fix: 'users'の正規表現を追加

--- src/main/java/com/workManagement/common/Const.java
+++ src/main/java/com/workManagement/common/Const.java
@@ -16,4 +16,16 @@ public class Const {
 
 	// セッション
 	public static final String SESSION_KEYWORD_ACCOUNT_BEAN = "SESSION_KEY1_ACCOUNT_BEAN";
+
+	// ユーザー役職
+	public static final String ROLE_USERS_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_USERS_GENERAL = "ROLE_GENERAL";
+
+	//-----------
+    // 正規表現
+    //-----------
+
+	// users
+	public static final String PATTERN_USERS_ADMIN_FLG_ROLE_ADMIN = "1";
+	public static final String PATTERN_USERS_DEL_FLG = "1";
 }
