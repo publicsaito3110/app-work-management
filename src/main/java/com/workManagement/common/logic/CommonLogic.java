e92fb8c7c18cddac353afcb8d649f29c4ed64d42
publicsaito3110 <public.business.saito3110@gmail.com>
2023-03-02 08:55:18 +0900

    add: Null空文字変換処理の追加

--- src/main/java/com/workManagement/common/logic/CommonLogic.java
+++ src/main/java/com/workManagement/common/logic/CommonLogic.java
@@ -10,12 +10,31 @@ public class CommonLogic {
 
 
 	/**
+	 * [共通 Logic] null空文字変換処理
+	 *
+	 * <p>
+	 * nullのとき空文字に変換する<br>
+	 * ただし、null以外のときは何もしない
+	 *
+	 * @param value 全ての値
+	 * @return String nullのとき空文字、null以外のときは何もしない
+	 */
+	public String changeEmptyByNull(String value) {
+
+		// nullのとき、空文字を返す
+		if (value == null) {
+			return "";
+		}
+		return value;
+	}
+
+
+	/**
 	 * [共通 Logic] バリデーション判定処理
 	 *
 	 * <p>
 	 * バリデーションを正規表現で判定し、バリデーションの結果を返す
 	 * ただし、正規表現パターンと一致していないまたは正規表現に則していないときは必ず失敗する
-	 * </p>
 	 *
 	 * @param value 全ての値
 	 * @param regex 正規表現パターン
@@ -103,25 +122,13 @@ public class CommonLogic {
 	 * [共通 Logic] 年月変換処理
 	 *
 	 * <p>
-	 * 年と月から対象の年月の最終日の日付を返す<br>
-	 * ただし、対象の年月が異常なときはnullを返す
+	 * 年と月から年月(YYYYMM)に変換して返す
 	 *
 	 * @param year 年
 	 * @param month 月
-	 * @return String 対象の年月の最終日の日付
+	 * @return String 年月
 	 */
 	public String changeYm(int year, int month) {
-
-		try {
-
-			// 最終日の日付を取得し、ymd(YYYYMMDD)に変換する
-			LocalDate localDate = LocalDate.of(year, month, 1);
-			int lastDay = localDate.lengthOfMonth();
-			return String.valueOf(year) + String.format("%02d", month);
-		} catch (Exception e) {
-
-			//例外発生時、nullを返す
-			return null;
-		}
+		return String.valueOf(year) + String.format("%02d", month);
 	}
 }
