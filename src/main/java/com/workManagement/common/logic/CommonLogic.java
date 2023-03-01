558fc6521404d0c93f8a6b4bd78dbea4cd1f891a
publicsaito3110 <public.business.saito3110@gmail.com>
2023-03-01 17:52:59 +0900

    fix: 共通日付変換処理の追加

--- src/main/java/com/workManagement/common/logic/CommonLogic.java
+++ src/main/java/com/workManagement/common/logic/CommonLogic.java
@@ -1,5 +1,7 @@
 package com.workManagement.common.logic;
 
+import java.time.LocalDate;
+
 /**
  * @author saito
  *
@@ -8,7 +10,7 @@ public class CommonLogic {
 
 
 	/**
-	 * バリデーション判定処理
+	 * [共通 Logic] バリデーション判定処理
 	 *
 	 * <p>
 	 * バリデーションを正規表現で判定し、バリデーションの結果を返す
@@ -36,6 +38,90 @@ public class CommonLogic {
 
 			//例外発生時、falseを返す
 			return false;
+		}
+	}
+
+
+	/**
+	 * [共通 Logic] LocalDate変換処理
+	 *
+	 * <p>
+	 * ymd(YYYYMMDD)をLocalDateで返す<br>
+	 * ただし、ymdがYYYYMMDDでない又は存在しない日付のときはnullを返す
+	 *
+	 * @param ymd (YYYYMMDD)
+	 * @return LocalDate ymdから変換されたLocalDate
+	 */
+	public LocalDate getLocalDateByYmd(String ymd) {
+
+		try {
+
+			// ymdがnullまたは8桁でないとき、nullを返す
+			if (ymd.length() != 8 ) {
+				return null;
+			}
+
+			// ymdをLocalDateに変換する
+			String ymdDate = ymd.substring(0, 4) + "-" + ymd.substring(4, 6) + "-" + ymd.substring(6, 8);
+			return LocalDate.parse(ymdDate);
+		} catch (Exception e) {
+
+			//例外発生時、nullを返す
+			return null;
+		}
+	}
+
+
+	/**
+	 * [共通 Logic] 日付変換処理
+	 *
+	 * <p>
+	 * 年と月から対象の年月の最終日の日付を返す<br>
+	 * ただし、対象の年月が異常なときはnullを返す
+	 *
+	 * @param year 年
+	 * @param month 月
+	 * @return String 対象の年月の最終日の日付
+	 */
+	public String changeLastDateYmd(int year, int month) {
+
+		try {
+
+			// 最終日の日付を取得し、ymd(YYYYMMDD)に変換する
+			LocalDate localDate = LocalDate.of(year, month, 1);
+			int lastDay = localDate.lengthOfMonth();
+			return String.valueOf(year) + String.format("%02d", month) + String.format("%02d", lastDay);
+		} catch (Exception e) {
+
+			//例外発生時、nullを返す
+			return null;
+		}
+	}
+
+
+	/**
+	 * [共通 Logic] 年月変換処理
+	 *
+	 * <p>
+	 * 年と月から対象の年月の最終日の日付を返す<br>
+	 * ただし、対象の年月が異常なときはnullを返す
+	 *
+	 * @param year 年
+	 * @param month 月
+	 * @return String 対象の年月の最終日の日付
+	 */
+	public String changeYm(int year, int month) {
+
+		try {
+
+			// 最終日の日付を取得し、ymd(YYYYMMDD)に変換する
+			LocalDate localDate = LocalDate.of(year, month, 1);
+			int lastDay = localDate.lengthOfMonth();
+			return String.valueOf(year) + String.format("%02d", month);
+		} catch (Exception e) {
+
+			//例外発生時、nullを返す
+			return null;
 		}
 	}
 }
