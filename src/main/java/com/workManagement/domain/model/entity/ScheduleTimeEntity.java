db6970b2eaab2293bfa7ca3c32d9e93f057a44f3
publicsaito3110 <public.business.saito3110@gmail.com>
2023-03-02 08:56:15 +0900

    add: スケジュール時間区分List変換処理の追加

--- src/main/java/com/workManagement/domain/model/entity/ScheduleTimeEntity.java
+++ src/main/java/com/workManagement/domain/model/entity/ScheduleTimeEntity.java
@@ -1,5 +1,11 @@
 package com.workManagement.domain.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.workManagement.common.logic.CommonLogic;
+import com.workManagement.domain.model.bean.collection.ScheduleTimeBean;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -110,4 +116,68 @@ public class ScheduleTimeEntity extends BaseEntity {
 
 	@Column(name = "rest_hm7")
 	private String restHm7;
+
+
+	/**
+	 * スケジュール時間区分List取得処理
+	 *
+	 * <p>
+	 * スケジュール時間区分をスケジュール時間区分ごとにListに変換する<br>
+	 * ただし、スケジュール時間区分が登録されていない場合はEmptyとなる
+	 *
+	 * @param void
+	 * @return List<ScheduleTimeBean> スケジュール時間区ごとに格納されたList
+	 *
+	 */
+	public List<ScheduleTimeBean> scheduleTimeFormatList() {
+
+		// 共通Logicクラス
+		CommonLogic commonLogic = new CommonLogic();
+
+		// スケジュール時間区分をListで格納
+		List<ScheduleTimeBean> ScheduleTimeBeanList = new ArrayList<>();
+
+		// スケジュール時間区分1が登録されているとき、スケジュール時間区分1を格納する
+		if (!commonLogic.changeEmptyByNull(name1).equals("") && !commonLogic.changeEmptyByNull(startHm1).equals("") && !commonLogic.changeEmptyByNull(endHm1).equals("") && !commonLogic.changeEmptyByNull(restHm1).equals("")) {
+			ScheduleTimeBean ScheduleTimeBean = new ScheduleTimeBean(name1, startHm1, endHm1, restHm1);
+			ScheduleTimeBeanList.add(ScheduleTimeBean);
+		}
+
+		//スケジュール時間区分2が登録されているとき、スケジュール時間区分2を格納する
+		if (!commonLogic.changeEmptyByNull(name2).equals("") && !commonLogic.changeEmptyByNull(startHm2).equals("") && !commonLogic.changeEmptyByNull(endHm2).equals("") && !commonLogic.changeEmptyByNull(restHm2).equals("")) {
+			ScheduleTimeBean ScheduleTimeBean = new ScheduleTimeBean(name2, startHm2, endHm2, restHm2);
+			ScheduleTimeBeanList.add(ScheduleTimeBean);
+		}
+
+		//スケジュール時間区分3が登録されているとき、スケジュール時間区分3を格納する
+		if (!commonLogic.changeEmptyByNull(name3).equals("") && !commonLogic.changeEmptyByNull(startHm3).equals("") && !commonLogic.changeEmptyByNull(endHm3).equals("") && !commonLogic.changeEmptyByNull(restHm3).equals("")) {
+			ScheduleTimeBean ScheduleTimeBean = new ScheduleTimeBean(name3, startHm3, endHm3, restHm3);
+			ScheduleTimeBeanList.add(ScheduleTimeBean);
+		}
+
+		//スケジュール時間区分4が登録されているとき、スケジュール時間区分4を格納する
+		if (!commonLogic.changeEmptyByNull(name4).equals("") && !commonLogic.changeEmptyByNull(startHm4).equals("") && !commonLogic.changeEmptyByNull(endHm4).equals("") && !commonLogic.changeEmptyByNull(restHm4).equals("")) {
+			ScheduleTimeBean ScheduleTimeBean = new ScheduleTimeBean(name4, startHm4, endHm4, restHm4);
+			ScheduleTimeBeanList.add(ScheduleTimeBean);
+		}
+
+		//スケジュール時間区分5が登録されているとき、スケジュール時間区分5を格納する
+		if (!commonLogic.changeEmptyByNull(name5).equals("") && !commonLogic.changeEmptyByNull(startHm5).equals("") && !commonLogic.changeEmptyByNull(endHm5).equals("") && !commonLogic.changeEmptyByNull(restHm5).equals("")) {
+			ScheduleTimeBean ScheduleTimeBean = new ScheduleTimeBean(name5, startHm5, endHm5, restHm5);
+			ScheduleTimeBeanList.add(ScheduleTimeBean);
+		}
+
+		//スケジュール時間区分6が登録されているとき、スケジュール時間区分6を格納する
+		if (!commonLogic.changeEmptyByNull(name6).equals("") && !commonLogic.changeEmptyByNull(startHm6).equals("") && !commonLogic.changeEmptyByNull(endHm6).equals("") && !commonLogic.changeEmptyByNull(restHm6).equals("")) {
+			ScheduleTimeBean ScheduleTimeBean = new ScheduleTimeBean(name6, startHm6, endHm6, restHm6);
+			ScheduleTimeBeanList.add(ScheduleTimeBean);
+		}
+
+		//スケジュール時間区分7が登録されているとき、スケジュール時間区分7を格納する
+		if (!commonLogic.changeEmptyByNull(name7).equals("") && !commonLogic.changeEmptyByNull(startHm7).equals("") && !commonLogic.changeEmptyByNull(endHm7).equals("") && !commonLogic.changeEmptyByNull(restHm7).equals("")) {
+			ScheduleTimeBean ScheduleTimeBean = new ScheduleTimeBean(name7, startHm7, endHm7, restHm7);
+			ScheduleTimeBeanList.add(ScheduleTimeBean);
+		}
+		return ScheduleTimeBeanList;
+	}
 }
