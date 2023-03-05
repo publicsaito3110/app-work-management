64d0d54e3982dc79ff4558821663d3d5ca7bec04
publicsaito3110 <public.business.saito3110@gmail.com>
2023-03-05 17:53:15 +0900

    fix: SQL:未退職ユーザー検索処理の追加

--- src/main/java/com/workManagement/domain/repository/UsersRepository.java
+++ src/main/java/com/workManagement/domain/repository/UsersRepository.java
@@ -1,5 +1,7 @@
 package com.workManagement.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -16,6 +18,7 @@ public interface UsersRepository extends BaseRepository<UsersEntity, String> {
 	/**
 	 * [Repository] ユーザ検索処理
 	 *
+	 * <p>
 	 * ユーザIDと一致するユーザを取得する<br>
 	 * ただし、一致するユーザーがいない場合はnullとなる
 	 *
@@ -29,6 +32,7 @@ public interface UsersRepository extends BaseRepository<UsersEntity, String> {
 	/**
 	 * [Repository] ユーザ検索処理
 	 *
+	 * <p>
 	 * ユーザIDまたはメールアドレスと一致するユーザを取得する<br>
 	 * ただし、一致するユーザーがいない場合はnullとなる
 	 *
@@ -38,4 +42,18 @@ public interface UsersRepository extends BaseRepository<UsersEntity, String> {
 	 */
 	@Query(value = "SELECT u.* FROM users u WHERE u.id = :id OR u.email = :email", nativeQuery = true)
 	public UsersEntity selectUsers(String id, String email);
+
+
+	/**
+	 * [Repository] ユーザ検索処理
+	 *
+	 * <p>
+	 * 未退職ユーザを全て取得する<br>
+	 * ただし、該当するユーザーがいない場合はnullとなる
+	 *
+	 * @param delFlg 退職フラグ
+	 * @return UsersEntity 全ての未退職ユーザ
+	 */
+	@Query(value = "SELECT u.* FROM users u WHERE u.del_flg != :delFlg OR u.del_flg IS NULL", nativeQuery = true)
+	public List<UsersEntity> selectUsersNotDelFlg(String delFlg);
 }
