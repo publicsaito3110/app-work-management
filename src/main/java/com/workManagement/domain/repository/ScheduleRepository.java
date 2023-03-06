4f3f943c1e4eb8ce70eb6cd4a7ad4f96eff7b9fe
publicsaito3110 <public.business.saito3110@gmail.com>
2023-03-06 20:32:05 +0900

    add: SQL:スケジュール検索処理の追加

--- src/main/java/com/workManagement/domain/repository/ScheduleRepository.java
+++ src/main/java/com/workManagement/domain/repository/ScheduleRepository.java
@@ -1,5 +1,7 @@
 package com.workManagement.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -26,4 +28,18 @@ public interface ScheduleRepository extends BaseRepository<ScheduleEntity, Integ
 	 */
 	@Query(value = "SELECT s.* FROM schedule s WHERE s.ym = :ym AND s.user = :user", nativeQuery = true)
 	public ScheduleEntity selectSchedule(String ym, String user);
+
+
+	/**
+	 * [Repository] 確定スケジュール検索処理
+	 *
+	 * <p>
+	 * 年月と一致する全てのユーザの確定スケジュールを取得する<br>
+	 * ただし、該当のスケジュールがない場合はEmptyとなる
+	 *
+	 * @param ym 取得したいスケジュールの年月
+	 * @return List<ScheduleEntity> 全てのユーザの確定スケジュール
+	 */
+	@Query(value = "SELECT s.* FROM schedule s WHERE s.ym = :ym", nativeQuery = true)
+	public List<ScheduleEntity> selectSchedule(String ym);
 }
